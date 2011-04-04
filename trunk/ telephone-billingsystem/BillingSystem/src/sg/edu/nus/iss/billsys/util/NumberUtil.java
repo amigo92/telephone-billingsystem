4f4466c6495fb63286d48dc2e08c531e24e5445e
sriragavan@gmail.com <sriragavan@gmail.com@5cfc198a-b972-bcd6-5376-fb4010c793c3>
2011-04-04 16:10:46 +0000

    added java docs comments

--- trunk/ telephone-billingsystem/BillingSystem/src/sg/edu/nus/iss/billsys/util/NumberUtil.java
+++ trunk/ telephone-billingsystem/BillingSystem/src/sg/edu/nus/iss/billsys/util/NumberUtil.java
@@ -1,6 +1,17 @@
 package sg.edu.nus.iss.billsys.util;
 
+/**
+ * 
+ * @author L Sriragavan
+ *
+ *This is the utility class contains number related methods.
+ */
 public class NumberUtil {
+	/**
+	 * Checks whether string value is a long value.
+	 * @param s String
+	 * @return Long
+	 */
 	public static boolean isLong(String s) {
 		try {
 			Long.parseLong(s);
--- trunk/ telephone-billingsystem/BillingSystem/src/sg/edu/nus/iss/billsys/util/StringUtil.java
+++ trunk/ telephone-billingsystem/BillingSystem/src/sg/edu/nus/iss/billsys/util/StringUtil.java
@@ -1,12 +1,27 @@
 package sg.edu.nus.iss.billsys.util;
 
 import sg.edu.nus.iss.billsys.constant.ComplaintStatus;
-
+/**
+ * 
+ * @author L Sriragavan
+ *
+ *This is the utility class contains String related methods.
+ */
 public class StringUtil {
+	
+	/**
+	 * Checks whether the given string is empty/ null.
+	 * @param s String
+	 * @return boolean
+	 */
 	public static boolean isNullOrEmpty(String s) {
 		return (s == null || "".equals(s)) ? true : false;
 	}
 	
+	/**
+	 * Returns the complaint status option values.
+	 * @return String array
+	 */
 	public static String[] getComplaintStatus() {
 		String[] array = new String[ComplaintStatus.values().length];
 		int i = 0;
@@ -14,7 +29,6 @@ public class StringUtil {
 			array[i] = status.toString();
 			i++;
 		}
-		System.out.println(">>>>>" + array.toString());
 		return array;
 	}
 
